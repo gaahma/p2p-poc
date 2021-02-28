d7690d62c1b3a3b892cd52f9b25fa5a10b6d749c
Adam Haag <adam@growers.ag>
2021-02-28 12:19:22 -0500

    removing chatty logs

--- index.js
+++ index.js
@@ -4,24 +4,14 @@ const defaults = require('dat-swarm-defaults')
 const getPort = require('get-port')
 const readline = require('readline')
 
-/**
- * Here we will save our TCP peer connections
- * using the peer id as key: { peer_id: TCP_Connection }
- */
 const peers = {}
-// Counter for connections, used for identify connections
-// let connSeq = 0
 
-// Peer Identity, a random hash for identify your peer
 const myUserName = process.argv[2] || 'Unknown user'
 const myId = crypto.randomBytes(32)
 // console.log('Your identity: ' + myId.toString('hex'))
 
 // reference to redline interface
 let rl
-/**
- * Function for safely call console.log with readline interface active
- */
 function log () {
   if (rl) {
     rl.clearLine()    
@@ -69,11 +59,9 @@ const sw = Swarm(config);
 
 
 (async () => {
-  // Choose a random unused port for listening TCP peer connections
   const port = await getPort()
 
   sw.listen(port)
-  // console.log('Listening to port: ' + port)
 
   // I'd really like to understand what hapens when this channel is joined
   sw.join('4401c223907732c197395e0669722183da5109508999cbe38279a04737bed728')
@@ -82,31 +70,28 @@ const sw = Swarm(config);
     const peerId = info.id.toString('hex')
 
     // Keep alive TCP connection with peer
-    if (info.initiator) {
-      try {
-        // these connections all close, idk what the value is of this...
-        conn.setKeepAlive(true, 2000)
-      } catch (exception) {
-        log('exception', exception)
-      }
-    }
+    // if (info.initiator) {
+    //   try {
+    //     // these connections all close , idk what the value is of this...
+    //     conn.setKeepAlive(true, 2000)
+    //   } catch (exception) {
+    //     log('exception', exception)
+    //   }
+    // }
 
     conn.on('data', (json) => {
-
       const { userName, message } = JSON.parse(json);
       log(`${userName}: ${message}`)
     });
 
-    conn.on('close', () => {
-      log(`closing connection with peer: ${peerId}`);
-      // delete peers[peerId];
-    });
+    // connections say they close all the damn time for no obvious reason, they're still open
+    // conn.on('close', () => {
+    //   log(`closing connection with peer: ${peerId}`);
+    //   delete peers[peerId];
+    // });
 
-    // Save the connection
-    // if (!peers[peerId]) {
+    // log(`Storing reference to peer: ${peerId}`)
     peers[peerId] = { conn };
-    // }
-    // peers[peerId].conn = conn
   });
 
   // Read user message from command line
