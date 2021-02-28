c297e5818bc53583d26017708e1bfd1df2bd7a34
Adam Haag <adam@growers.ag>
2021-02-28 14:01:22 -0500

    refactoring block class

--- blocks/Block.js
+++ blocks/Block.js
@@ -1,14 +1,18 @@
 import { SHA256 } from 'crypto-js';
 
 export class Block {
-  constructor (index, timestamp, data, precedingHash=null) {
+  constructor ({ index, timestamp, data, precedingHash, hash }) {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
-    this.precedingHash = precedingHash;
-    this.hash = this.computeHash();    
+    this.precedingHash = precedingHash || this.initHash();
+    this.hash = hash || this.computeHash();
    }
-   computeHash(){
-       return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString();
-   } 
+
+   initHash () {
+      return SHA256(this.index + this.timestamp + JSON.stringify(this.data)).toString();
+   }
+   computeHash () {
+      return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString();
+   }
 }
