3bfff5f9797282e64051746fb7cdf25f26dae6b8
Adam Haag <adam@growers.ag>
2021-02-28 13:43:43 -0500

    fixed named export

--- blocks/BallotBlock.js
+++ blocks/BallotBlock.js
@@ -1,6 +1,6 @@
 import { Block } from './Block';
 
-export class VoterBlock extends Block {
+export class BallotBlock extends Block {
   constructor() {
     this.type = 'BALLOT',
     super(arguments);
